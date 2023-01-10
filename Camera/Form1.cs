caf16e80b36468f6a0a06f3235381b84050bd9a8
phongjr2001 <vudinhphong982001@gmail.com>
2023-01-10 09:03:11 +0700

    cam

---
+++ Camera/Form1.cs
@@ -0,0 +1,78 @@
+﻿using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Camera
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection cameras;
+        private VideoCaptureDevice cam;
+        public Form1()
+        {
+            InitializeComponent();
+            cameras = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo info in cameras)
+            {
+                comboBox1.Items.Add(info.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (cam != null && cam.IsRunning)
+            {
+                cam.Stop();
+            }
+            cam = new VideoCaptureDevice(cameras[comboBox1.SelectedIndex].MonikerString);
+            cam.NewFrame += Cam_NewFrame;
+            cam.Start();
+        }
+
+        private void Cam_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = bitmap;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.InitialDirectory = "F:\\Thực tập cs cn\\Images";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(saveFileDialog1.FileName);
+            }
+        }
+
+        //protected override void OnClosed(EventArgs e)
+        //{
+        //    base.OnClosed(e);
+        //    if (cam != null && cam.IsRunning)
+        //    {
+        //        cam.Stop();
+        //    }
+        //}
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (cam != null && cam.IsRunning)
+            {
+                cam.Stop();
+            }
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
